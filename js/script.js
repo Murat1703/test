97b9d082c81643c0f901530cf387b2f52e7c8959
Murat Kuzhakhmetov <105972017+Murat1703@users.noreply.github.com>
2022-06-11 14:25:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let burger = document.querySelector('.menu-burger');
+let menu = document.querySelector('.menu-mobile');
+let bodyLck = document.querySelector('body');
+burger.addEventListener('click', function()
+	{
+		burger.classList.toggle('active');
+		menu.classList.toggle('active');
+		bodyLck.classList.toggle('lock');
+	})
+$(document).ready(function()
+{
+ AOS.init();
+
+	$(window).scroll(function(){
+	if ($(this).scrollTop() > 100) {
+	$('.scrollup').fadeIn();
+	} else {
+	$('.scrollup').fadeOut();
+	}
+	});
+	 
+	$('.scrollup').click(function(){
+	$("html, body").animate({ scrollTop: 0 }, 600);
+	return false;
+	});
+
+	 $("a").on('click', function(event) {
+
+    // Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+    if (this.hash !== "") {
+      // Запретить поведение щелчка якоря по умолчанию
+      event.preventDefault();
+
+      // Хранить хэш
+      var hash = this.hash;
+
+      // Использование метода animate() jQuery для добавления плавной прокрутки страницы
+      // Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+        window.location.hash = hash;
+      });
+    } // Конец, если
+  });
+ 
+});
+
